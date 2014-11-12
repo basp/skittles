267ddf520b8ff0a85bfeda6ad82bf0c3174f4958
Bas Pennings <baspster@gmail.com>
2014-11-12 16:59:41 +0100

    Another example

--- examples.sql
+++ examples.sql
@@ -1,3 +1,7 @@
 select *
 from [skittles].[documents]
 where [data].exist('/event/tags/tag[text() = "foo"]') = 1
+
+select *
+from [skittles].[documents]
+where [data].exist('/event/msg/text()[contains(., "quux")]') = 1
